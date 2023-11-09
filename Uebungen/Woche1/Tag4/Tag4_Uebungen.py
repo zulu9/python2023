ebc2995f403337dc26f792a785de533521c4d713
zulu9 <thmskntt@gmail.com>
2023-11-09 12:15:59 +0100

    write tests

--- Uebungen/Woche1/Tag4/Tag4_Uebungen.py
+++ Uebungen/Woche1/Tag4/Tag4_Uebungen.py
@@ -1,8 +1,15 @@
-myfile = open('./bsp.txt','r')
+myfile = open('./bsp.txt', 'r')
 print(myfile.readlines())
+myfile = open('./bsp.txt', 'r')
 myfile.close()
-myfile = open('./bsp.txt','r')
+myfile = open('./bsp.txt', 'r')
 print(myfile.read(3))
 print(myfile.read(5))
 print(myfile.read())
 myfile.close()
+
+myinput = input("Input: >> ")
+print(myinput+"5")
+
+outputfile = open('./bsp2.txt', 'w')
+outputfile.write("Hallo Erde!")
