65a876b99f6aad8e7cc145320c3ac54fc4d0c804
zulu9 <thmskntt@gmail.com>
2023-11-13 09:08:33 +0100

    uebung1

--- Uebungen/Woche2/Tag1.py
+++ Uebungen/Woche2/Tag1.py
@@ -1,1 +1,18 @@
-#Loops
+# Loops
+my_list = [1, 2, 3, "MÖP", 5]
+for i in my_list:
+    print(i)
+
+print("\nMit Range:")
+for i in range(0, len(my_list)):
+    print(i)
+
+print("\nTiere")
+zoo = {"Affe": "klug", "Elefant": "groß", "Tiger": "gefährlich", "Pinguin":"Linuxfan"}
+for tier in zoo:
+    print(tier, "ist", zoo[tier])
+
+print("\nBuchstaben aus Name")
+name="Karl Ranseier,"
+for buchstabe in name:
+    print(buchstabe)
