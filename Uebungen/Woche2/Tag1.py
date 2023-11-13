999f5c5f51042ad5502c674f043b35a27cd15c39
zulu9 <thmskntt@gmail.com>
2023-11-13 09:29:56 +0100

    uebung1

--- Uebungen/Woche2/Tag1.py
+++ Uebungen/Woche2/Tag1.py
@@ -1,18 +1,28 @@
 # Loops
-my_list = [1, 2, 3, "MÖP", 5]
+my_list = [1, 2, 3, "MÖP", 5, [6, "sieben"], [8, 9, "zehn"]]
+print("\nNormal")
 for i in my_list:
     print(i)
-
 print("\nMit Range:")
 for i in range(0, len(my_list)):
     print(i)
+print("\nVerschachtelt")
+my_list2 = [[1], "doof", "3", ["MÖP"], [5], [6, "sieben"], [8, 9, "zehn"]]
+for i in my_list2:
+    for m in i:
+        print(m)
 
 print("\nTiere")
-zoo = {"Affe": "klug", "Elefant": "groß", "Tiger": "gefährlich", "Pinguin":"Linuxfan"}
-for tier in zoo:
-    print(tier, "ist", zoo[tier])
+was_ist = {"Affe": "klug", "Elefant": "groß", "Tiger": "gefährlich", "Pinguin": "Linuxfan"}
+for tier in was_ist:
+    print(tier, "ist", was_ist[tier])
 
 print("\nBuchstaben aus Name")
-name="Karl Ranseier,"
+name = "Karl Ranseier"
 for buchstabe in name:
     print(buchstabe)
+
+print("\nBuchstaben aus Name mit Nummer")
+name = "Ben Utzer"
+for buchstabennummer in range(len(name)):
+    print(buchstabennummer+1, "\t", name[buchstabennummer])
