afd3678a24e8580ed4e95055d9efe9a0a9a680ee
zulu9 <thmskntt@gmail.com>
2023-11-16 09:03:45 +0100

    IDs

--- Uebungen/Woche2/Tag9/Tag9_Tests.py
+++ Uebungen/Woche2/Tag9/Tag9_Tests.py
@@ -1,4 +1,14 @@
 # Tag 9 Tests
 
-x = 3
+# IDs
+x = int(3)
 print("X: ", id(x))
+y = 3.123232
+print("Y: ", id(y))
+z = -14
+z2 = 5000
+print("Z, Z2, 3, 3.123232: ", id(z), id(z2), id(3), id(3.123232))
+string = "Helllo"
+print(id(string))
+string = "World"
+print(id(string))
