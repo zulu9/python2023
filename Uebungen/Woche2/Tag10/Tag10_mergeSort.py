3e5267b10c44b1f132271a6f69d7135517d36158
zulu9 <thmskntt@gmail.com>
2023-11-17 14:19:57 +0100

    Merge sort

--- Uebungen/Woche2/Tag10/Tag10_mergeSort.py
+++ Uebungen/Woche2/Tag10/Tag10_mergeSort.py
@@ -5,21 +5,24 @@ import math
 
 def merge_sort(
         liste: list,
-        level: int = 0):
+        level: int = 0,
+        seite: str = "Main"):
     """
     Rekursives Merge Sort. ACHTUNG: Arbeitet an der Orignalliste. Kein Return!
     :param liste: Liste die sortiert werden soll
     :param level: Momentane Tiefe der Rekursion
+    :param seite: Auf welcher Seite (Links oder Rechts) sind wir gerade?
     :return: gibbet nicht
     """
     level += 1
     if len(liste) > 1:
-        mitte = len(liste) // 2
+        mitte = math.ceil((len(liste) / 2))
         links = liste[:mitte]
         rechts = liste[mitte:]
 
         print("--------------------------------------------",
               "\n\tLevel:\t", level,
+              "\n\tSeite:\t", seite,
               "\n\tOuter:\t", liste,
               "\n\tLinks:\t", links,
               "\n\tRechts:\t", rechts,
@@ -27,8 +30,8 @@ def merge_sort(
               )
 
         # Für beide Teillisten die Funktion wieder aufrufen
-        merge_sort(links, level)
-        merge_sort(rechts, level)
+        merge_sort(links, level, "Links")
+        merge_sort(rechts, level, "Rechts")
 
         # Zähler für die Sublisten
         l_num = 0
@@ -63,9 +66,9 @@ def merge_sort(
             outer_num += 1
 
 
-randomlist = [70, 50, 30, 10, 20, 40, 60]
-upto = 7  # Anzahl der Zufallszahlen zwischen -100 und +100, die erzeugt werden sollen
-#randomlist = [random.randrange(-100, 100, 1) for i in range(upto)]
+# randomlist = [38, 27, 43, 3, 9, 82, 10]
+upto = 5  # Anzahl der Zufallszahlen zwischen -100 und +100, die erzeugt werden sollen
+randomlist = [random.randrange(-100, 100, 1) for i in range(upto)]
 unsortiert = randomlist.copy()  # Unosortierte Liste merken, weil wir glech am Original rumfuschen
 merge_sort(randomlist)
 print("Unsotiert:\t\t", unsortiert)
