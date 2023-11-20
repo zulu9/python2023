457c233f25de3f78573abdec146c5526ed31ea15
zulu9 <thmskntt@gmail.com>
2023-11-20 10:58:56 +0100

    reduce()

--- Uebungen/Woche3/Tag11/Tag11_Tests.py
+++ Uebungen/Woche3/Tag11/Tag11_Tests.py
@@ -1,1 +1,14 @@
 # Tag 11 Tests
+import random
+from functools import reduce
+
+# Reduce
+liste = []
+anzahl = 20
+maxzahl = 10
+for i in range(anzahl):
+    liste.append(random.randrange(maxzahl) + 1)
+
+myfun = reduce(lambda x, y: x * y, liste)
+print(liste)
+print(myfun)
