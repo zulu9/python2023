ab0c288e937e1c3d01e73160a8539eb2b4fa0ac9
zulu9 <thmskntt@gmail.com>
2023-11-20 16:33:12 +0100

    Aufgabe 7

--- Uebungen/Woche3/Tag11/Tag11_Uebungen.py
+++ Uebungen/Woche3/Tag11/Tag11_Uebungen.py
@@ -1,5 +1,5 @@
 # Tag 11 Übungen
-
+from functools import reduce
 # Aufgabe 1
 print("\nAufgabe 1")
 
@@ -27,7 +27,7 @@ with open("woerter_bereinigt.txt", 'w') as f:
 print("\nAufgabe 3")
 laengen = {wort: len(wort) for wort in capitalized_wordlist}
 print(laengen)
-matches = list(filter(lambda wort: any(substr in wort for substr in ["m", "fe", "dr"]), capitalized_wordlist))
+matches = list(filter(lambda mywort: any(substr in mywort for substr in ["m", "fe", "dr"]), capitalized_wordlist))
 print(matches)
 
 # Aufgabe 4
@@ -53,6 +53,7 @@ def lamesearch(keyword: str, inputstring: str) -> int:
             count += 1
     return count
 
+
 with open("zen.txt") as f:
     contents = f.read()
 
@@ -66,8 +67,36 @@ with open("zahlen.txt") as f:
     contents = f.read()
 contentlist = list(contents)
 clean_contentlist = list(filter(lambda s: s.strip(","), contentlist))
-integer_contentlist = list(map(int,clean_contentlist))
+integer_contentlist = list(map(int, clean_contentlist))
 print(integer_contentlist)
+
 
 # Aufgabe 7
 print("\nAufgabe 7")
+# integer_contentlist[0]="pferd" # Cause Error
+
+
+def mysumme(liste: list) -> int:
+    summe = 0
+    try:
+        summe = reduce(lambda x, y: x + y, liste)
+    except TypeError:
+        summe = "Error"
+    finally:
+        return summe
+
+
+def myproduct(liste: list) -> int:
+    product = 1
+    try:
+        product = reduce(lambda x, y: x * y, liste)
+    except TypeError:
+        product = "Error"
+    finally:
+        return product
+
+
+print(mysumme(integer_contentlist))
+print(mysumme(integer_contentlist) / len(integer_contentlist))
+print(myproduct(integer_contentlist))
+print(myproduct(list(filter(lambda element: element != 0, integer_contentlist))))
