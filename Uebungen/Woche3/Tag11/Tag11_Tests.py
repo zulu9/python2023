fff1cea66788b81af0242a078ff2657154691bef
zulu9 <thmskntt@gmail.com>
2023-11-20 11:25:44 +0100

    try, except

--- Uebungen/Woche3/Tag11/Tag11_Tests.py
+++ Uebungen/Woche3/Tag11/Tag11_Tests.py
@@ -12,3 +12,14 @@ for i in range(anzahl):
 myfun = reduce(lambda x, y: x * y, liste)
 print(liste)
 print(myfun)
+
+# Try und except
+myfun = "Hello"
+try:
+    print(myfun)
+except Exception: # Broad exception. Nich machen!
+    print("Something went wrong!")
+else:
+    print("Everything works fine")
+finally:
+    print("Wir sind durch")
