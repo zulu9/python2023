43a5c72ae8b3f811a746f80ec78457b2779dcd05
zulu9 <thmskntt@gmail.com>
2023-11-24 02:29:52 +0100

    fixed player movement logic

--- gameproject/Xventure_terminal_oop/xventure.py
+++ gameproject/Xventure_terminal_oop/xventure.py
@@ -2,26 +2,37 @@
 # Needs:
 #   * python 3.x + some Modules
 #   * UTF-8 Terminal Emulator with Noto Color Emoji Font or similar(TODO TEST ON WINDOWS)
-#   * On Linux: root due to used Keyboard-Libary (FIXME FIND BETTER LIBRARY)
-#  BROKEN FIXME TODO
 
 # ##------IMPORTS------## #
 import numpy as np
 import os
+import sys
 import time
-from pynput import keyboard
-import playsound
 import random
-
+from threading import Thread
+from playsound import playsound
+from pynput import keyboard
 
 # ##------GLOBAL FUNCTIONS------## #
 # Keybord handling
+
+
 def on_press(key):
+    """
+    Handle key presses
+    :param key:
+    :return:
+    """
     global Keyboard_Input
     Keyboard_Input = key
 
 
-def on_release(key):
+def on_release(key):  # FIXME DOES NOT WORK AND MIGHT CAUSE DOUBLE EVENTS
+    """
+    handle key releases
+    :param key:
+    :return:
+    """
     global Keyboard_Input
     if str(key) == "Key.Q":
         exit(0)
@@ -34,6 +45,19 @@ listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
+# //END Initialize Keyboard listener
+
+# Sound Handling
+
+
+def play_soundfile(path):
+    """
+
+    :param path: Path to soundfile
+    :return: no return. starts thread
+    """
+    play_thread = Thread(target=playsound(path, False), daemon=True)
+    play_thread.start()
 
 
 def fullwidth_str(text: str) -> str:
@@ -66,6 +90,13 @@ def clear():
 
 class Entitiy:
     def __init__(self, name: str, type_id: int, position: tuple, attributes: list):
+        """
+
+        :param name: Name of the entity
+        :param type_id: Type (as integer ID) of the entity as defined in grid-file or function
+        :param position: position of the entity on the grid
+        :param attributes:  list of additional attributes (currently unused)
+        """
         self.name = name
         self.type_id = type_id
         self.position = position
@@ -91,8 +122,8 @@ class Entitiy:
         if (
                 new_position[0] != 0
                 and new_position[1] != 0
-                and new_position[0] < my_grid.size_x
-                and new_position[1] < my_grid.size_y):
+                and new_position[0] < current_grid.size_x
+                and new_position[1] < current_grid.size_y):
             self.position = new_position
             return new_position
         else:
@@ -103,18 +134,22 @@ class Player(Entitiy):
     pass
 
 
-class Graphics:
+class Graphicset:
     def __init__(self, name: str):
+        """
+
+        :param name: Name of the graphicsset. Must be placed in $PWD/res/$name.grf
+        """
         self.name = name
         self.assignments = {}
 
     def read_from_file(self):  #
         """
         Read assignments from Textfile in the res subdirectory (filename.grf)
-        :return:  Graphic assignments read from file
+        :return:  Graphicset assignments
         """
-        graphics_file = os.path.dirname(__file__) + '/res/' + self.name + '.grf'
-        with open(graphics_file, encoding="utf-8", mode='r') as graphicsfile:  # TODO Make sure it works on Windows too
+        graphicset_file = os.path.dirname(__file__) + '/res/' + self.name + '.grf'
+        with open(graphicset_file, encoding="utf-8", mode='r') as graphicsfile:  # TODO Make sure it works on Windows
             try:
                 for lines in graphicsfile:
                     key, value = lines.strip().split(':')  # An : trennen
@@ -131,25 +166,24 @@ class Grid:
         A grid in the game
         :param grid_name: (file)name of the grid
         :param graphics_name: (file)name of the graphics assignment
-        :param size_x: gridsize X
-        :param size_y: gridsize Y
+        :param size_x: gridsize X in utf-8 full-width chars
+        :param size_y: gridsize Y in utf-8 full-width chars
         """
         self.name = grid_name
         self.size_x = size_x
         self.size_y = size_y
-        self.graphics = Graphics(graphics_name).read_from_file()
+        self.graphics = Graphicset(graphics_name).read_from_file()
         self.values = []
 
     def create_rectangle(self, static_objects: list = None):
         """
         Create a rectangular grid
-        :return: Rectangle of 0s, surrounded by 1s
+        :return: Rectangle of 0s, surrounded by 1s and insert values for static game objects
         """
 
         # Create empty grid
         self.values = np.ones((self.size_x + 1, self.size_y + 1), dtype=np.int8)
         self.values[1:-1, 1:-1] = 0  # Free space
-
         # Insert static objects like decorations
         if static_objects is not None:
             for thing in static_objects:
@@ -158,7 +192,7 @@ class Grid:
 
     def create_from_file(self):
         """
-        Create a grid from a Textfile in the res subdirectory (filename.lvl)
+        Create a grid from a Textfile in the res subdirectory ($PWD/res/$grid.name.lvl)
         :return:  Grid read from file
         """
         grid_file = os.path.dirname(__file__) + '/res/' + self.name + '.lvl'
@@ -170,13 +204,16 @@ class Grid:
                     self.values.append(elements)
             except ValueError:
                 self.values = None
+            else:
+                self.size_x = len(self.values)
+                self.size_y = len(self.values[0])
             finally:
                 return self
 
     def get_random_position(self) -> tuple[int, int]:
         """
-
-        :return:  A random position inside the grid
+        A random position inside the grid
+        :return:  tuple with the random position
         """
         g_random_position = (random.randrange(self.size_x - 1) + 1, random.randrange(self.size_y - 1) + 1)
         return g_random_position
@@ -185,13 +222,15 @@ class Grid:
         """
         Updates the grid on screen
         :param p_input: Player input
-        :return: no return
+        :return: no return, just updates stuff
         """
 
         # Move player
         if p_input is not None:
-            my_player.move(p_input)
+            self.values[my_player.position] = 0  # Free up old position
+            my_player.move(p_input)  # Move Player
 
+        self.values[my_player.position] = my_player.type_id  # FIXME WARNING ABOUT TYPES: NUMPY?
         # Move movable entities
         # ....
 
@@ -200,10 +239,10 @@ class Grid:
 
         # Upgrade the grid
 
-    def paint(self, graphics: Graphics):
+    def paint(self, graphicset: Graphicset):
         """
         # Actually paint the grid to screen
-        :param graphics: graphics assignment to be used
+        :param graphicset: graphics assignment to be used
         :return: no return. outputs directly to terminal
         """
         clear()
@@ -211,11 +250,8 @@ class Grid:
         el_num = 0
         for row in self.values:
             for element in row:
-                # Draw Player
-                if (row_num, el_num) == my_player.position:
-                    element = 2
                 # Check the graphics assignment dict and replace elements with emojis etc
-                element = graphics.assignments.get(element)
+                element = graphicset.assignments.get(element)
                 print(format(str(element), "<1"), end="")
                 el_num += 1
             el_num = 0
@@ -235,29 +271,28 @@ default_update_speed = 0.1
 start_time = time.time()  # Start the clock
 
 #  Create grid
-# my_grid = Grid('level1').create_from_file()
-my_grid = Grid('ff_level1', "ff_day", 30, 30).create_rectangle()
+# current_grid = Grid('ff_level1', 'ff_day').create_from_file()
+current_grid = Grid('ff_level1', "ff_snakemode", 30, 30).create_rectangle()
 
 # Create Entities
 my_decorations = []
-number_of_decorations = 30
+number_of_decorations = (current_grid.size_x + current_grid.size_y) // 2
 for i in range(0, number_of_decorations):
     my_decorations.append(
-        Entitiy("decoration" + str(i), random.choice([3, 4, 5]), my_grid.get_random_position(), [])
+        Entitiy("decoration" + str(i), random.choice([3, 4, 5]), current_grid.get_random_position(), [])
     )
-my_grid.create_rectangle(my_decorations)
+current_grid.create_rectangle(my_decorations)  # Add decorations to current grid
 
 
-my_player = Entitiy("Kisa", 2, my_grid.get_random_position(), [])
-playsound.playsound('./res/music.mp3', False)
+my_player = Entitiy("Kisa", 2, (1, 1), [])
 
-my_grid.create_rectangle(my_decorations)
-print(my_grid.values)
+# Start background music
+play_soundfile('./res/music.mp3')
 
 #  Evaluate Keyboard input and update grid
 while True:
     try:
-        print(Keyboard_Input)
+        # print(Keyboard_Input)
         # Keyboard Eingaben
         if str(Keyboard_Input) == "Key.left":
             player_input = 'left'
@@ -267,7 +302,7 @@ while True:
             player_input = 'up'
         elif str(Keyboard_Input) == "Key.down":
             player_input = 'down'
-        elif str(Keyboard_Input) == "Key.q":
+        elif str(Keyboard_Input) == "Key.q":  # FIXME Does not react to q. Only CTRL+C works
             break
         else:
             player_input = None
@@ -280,9 +315,10 @@ while True:
         # my_grid.graphics.read_from_file()
 
         # Update grid and draw on screen
-        my_grid.update(player_input)
-        my_grid.paint(my_grid.graphics)
+        current_grid.update(player_input)
+        current_grid.paint(current_grid.graphics)
         time.sleep(default_update_speed)
 
     except KeyboardInterrupt:  # CTRL-C was pressed
-        break
+        print("BYE")
+        sys.exit()
