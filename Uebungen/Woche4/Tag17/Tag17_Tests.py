4eb2531c1ea59a416d1d7cc876433d0997bb7ded
zulu9 <thmskntt@gmail.com>
2023-11-28 08:51:28 +0100

    addes Tag17_Tests.py GUIs mit tkinter

--- Uebungen/Woche4/Tag17/Tag17_Tests.py
+++ Uebungen/Woche4/Tag17/Tag17_Tests.py
@@ -2,4 +2,14 @@
 import tkinter as tk
 
 root = tk.Tk()
+
+root.title("My Windows")
+
+window_width = 1280
+window_height = 1024
+
+centerX = 200
+centerY = 200
+root.geometry(f"{window_width}x{window_height}+{centerX}+{centerY}")
+
 root.mainloop()
